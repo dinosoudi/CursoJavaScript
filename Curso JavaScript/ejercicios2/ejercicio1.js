a373d0b11cb27374db8245c02cebffc73f4e8e96
dinosoudi <90942593+dinosoudi@users.noreply.github.com>
2024-10-28 13:57:13 -0600

    Update ejercicio1.js
    solucion de meter numero y no letras

--- Curso JavaScript/ejercicios2/ejercicio1.js
+++ Curso JavaScript/ejercicios2/ejercicio1.js
@@ -1,22 +1,27 @@
 'use strict'
 
 // pedir 2 numeros, decir mayor, menor y si son iguales
+// solucionar que sean numeros los valores entrantes y no letras porque da error
 
 // se dan los 2 numeros
-var num1 = parseInt( prompt('Dame el primer numero: ',0) );
-var num2 = parseInt( prompt('Dame el segundo numero: ',0) );
+var num1;
+var num2;
 var iguales;
 var mayor;
 var menor;
 
+// pide 2 numeros 
+do{
+    num1 = parseInt( prompt('Dame el primer numero: ',0) );
+    num2 = parseInt( prompt('Dame el segundo numero: ',0) );
+}while(isNaN(num1) || isNaN(num2));
+
 // imprime si son iguales
 if (num1 == num2){
-    console.log("son iguales");
-    alert("los numeros son iguales");
+    alert("los numeros son iguales: " + num1 + " y " + num2);
     iguales = true;
 } else{
-    console.log("son diferentes");
-    alert("los numeros son diferentes")
+    alert("los numeros son diferentes: " + num1 + " y " + num2)
     iguales = false;
 }
 
